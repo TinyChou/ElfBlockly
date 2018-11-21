7ab511e7e358e4d946f71ffb064c6b8f07f19697
tinychou <tinychou@tinys-MacBook-Pro.local>
2018-11-21 09:25:41 +0800

    initial commit for project hierarchy using rollup.

---
+++ test/test.js
@@ -0,0 +1,5 @@
+const assert = require('assert');
+const ElfBlockly = require('..');
+
+assert.equal(ElfBlockly.VERSION, '0.0.1');
+console.log(`\u001B[32m✓\u001B[39m ElfBlockly.VERSION: ${ElfBlockly.VERSION}`);
